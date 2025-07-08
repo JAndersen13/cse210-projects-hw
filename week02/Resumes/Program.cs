261e8da5b8ee3057dbbaa877ba9c40d8a8933cac
JAndersen13 <jowens@byupathway.edu>
2025-07-07 19:02:04 -0700

    Learning assignment for week 2

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -4,6 +4,22 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        Job job1 = new Job();
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Microsoft";
+        job1._startYear = "217";
+        job1._endYear = "2020";
+
+        Job job2 = new Job();
+        job2._jobTitle = "Mechanical Engineer";
+        job2._company = "Apple";
+        job2._startYear = "2020";
+        job2._endYear = "2025";
+
+        Resume resume1 = new Resume();
+        resume1._name = "Steve Jones";
+        resume1._jobs.Add(job1);
+        resume1._jobs.Add(job2);
+        resume1.Display();
     }
 }
